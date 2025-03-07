8ae49a9d67a58bbec1ca388a56a9681cdc5c9169
28schellerj <28schellerj@bwschools.net>
2025-03-07 08:09:23 -0500

    commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import turtle
+
+screen=turtle.Screen()
+screen.bgcolor("pink")
+
+olympic_rings =turtle.Turtle()
+olympic_rings.speed(5)
+
+radius=35
+
+olympic_rings.penup()
+olympic_rings.goto(-80,15)
+olympic_rings.pendown()
+olympic_rings.pencolor("blue")
+olympic_rings.circle(35)
+
+olympic_rings.penup()
+olympic_rings.goto(0,15)
+olympic_rings.pendown()
+olympic_rings.pencolor("yellow")
+olympic_rings.circle(35)
+
+olympic_rings.penup()
+olympic_rings.goto(80,15)
+olympic_rings.pendown()
+olympic_rings.pencolor("black")
+olympic_rings.circle(35)
+
+olympic_rings.penup()
+olympic_rings.goto(-40,-20)
+olympic_rings.pendown()
+olympic_rings.pencolor("green")
+olympic_rings.circle(35)
+
+olympic_rings.penup()
+olympic_rings.goto(40,-20)
+olympic_rings.pendown()
+olympic_rings.pencolor("red")
+olympic_rings.circle(35)
+olympic_rings.pencolor("purple")
+olympic_rings.write("Winter Olympics",font=("Ariel",24, "bold"),align="center")
+olympic_rings.write("2026",font=("Ariel",24, "bold"),align="center")
+
+turtle.done()
